0686ad68f4d42d5b7fb6f0a11fb4038b9b779948
zhengx55 <williamzhxin@gmail.com>
2023-11-03 11:34:53 -0400

    test lint #2

--- .husky/pre-commit.sh
+++ .husky/pre-commit.sh
@@ -2,4 +2,4 @@
 . "$(dirname -- "$0")/_/husky.sh"
 
 #! npm run cypress:run #! Needs DEV to be running
-pnpm run lint-staged
+pnpm lint-staged
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
 
 
 
+
+
+
  
   
 }
