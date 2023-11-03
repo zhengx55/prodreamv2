4c09f6a176bc12f5f1cc27f8b9acdb93d397265d
zhengx55 <williamzhxin@gmail.com>
2023-11-03 11:22:38 -0400

    add lint-stage and huskey#3

--- .husky/pre-commit.sh
+++ .husky/pre-commit.sh
@@ -2,4 +2,4 @@
 . "$(dirname -- "$0")/_/husky.sh"
 
 #! npm run cypress:run #! Needs DEV to be running
-npm run lint-staged
+pnpm run lint-staged
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,6 @@ module.exports = {
     // this will check Typescript files
     "**/*.(ts|tsx)": () => "pnpm tsc --noEmit",
 
-    // This will lint and format TypeScript and                                             //JavaScript files
     "**/*.(ts|tsx|js)": (filenames) => [
         `pnpm eslint --fix ${filenames.join(" ")}`,
         `pnpm prettier --write ${filenames.join(" ")}`,
