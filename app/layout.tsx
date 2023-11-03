2e1b67c290a48075f7fab51266ab4e38058740e8
zhengx55 <williamzhxin@gmail.com>
2023-11-03 13:54:50 -0400

    tanstack provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { TanstackProvider } from '@/context/TanstackProvider';
 import './globals.css';
 import type { Metadata } from 'next';
 
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <TanstackProvider>{children}</TanstackProvider>
+      </body>
     </html>
   );
 }
