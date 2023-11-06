34fbd606bd82846990d3ec0d1855e97ecc6217e5
zhengx55 <williamzhxin@gmail.com>
2023-11-06 15:28:47 -0500

    remove lang router

--- middleware.ts
+++ middleware.ts
@@ -24,23 +24,23 @@ function getLocale(request: NextRequest): string | undefined {
 }
 
 export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  // Check if there is any supported locale in the pathname
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
-    return NextResponse.rewrite(
-      new URL(
-        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
-        request.url
-      )
-    );
-  }
+  // const pathname = request.nextUrl.pathname;
+  // // Check if there is any supported locale in the pathname
+  // const pathnameIsMissingLocale = i18n.locales.every(
+  //   (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  // );
+  // // Redirect if there is no locale
+  // if (pathnameIsMissingLocale) {
+  //   const locale = getLocale(request);
+  //   // e.g. incoming request is /products
+  //   // The new URL is now /en-US/products
+  //   return NextResponse.rewrite(
+  //     new URL(
+  //       `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
+  //       request.url
+  //     )
+  //   );
+  // }
 }
 
 export const config = {
--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   compress: true,
   // distDir: 'build',
-  poweredByHeader: false,
+  // poweredByHeader: false,
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
