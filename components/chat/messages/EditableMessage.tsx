d0d2932a38ee7c953c8f69380fc8470080c15391
zhengx55 <williamzhxin@gmail.com>
2023-11-13 16:50:47 -0500

    activity message box

--- components/chat/messages/EditableMessage.tsx
+++ components/chat/messages/EditableMessage.tsx
@@ -5,9 +5,10 @@ type Props = {};
 
 const EditableMessage = (props: Props) => {
   return (
-    <div className='flex w-[80%] flex-col self-start rounded-[20px] bg-shadow-200 p-5'>
+    <div className='flex w-[80%] min-w-[485px] flex-col self-start rounded-[20px] bg-shadow-200 p-5'>
       <p className='base-regular text-black-700'>
         Here is the description for your Extracurricular activity!
+        <div className='mt-4 h-60 w-full rounded-[10px] bg-white shadow-panel'></div>
         <div className='flex-between mt-4 flex w-full'>
           <div className='flex gap-x-2'>
             <Button variant={'bold'} size={'sm'}>
