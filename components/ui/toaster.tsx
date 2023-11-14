438c34e84d4712f88407d8147de4e4f0c84fa109
zhengx55 <williamzhxin@gmail.com>
2023-11-13 19:34:33 -0500

    toaster icon

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -9,6 +9,7 @@ import {
   ToastViewport,
 } from '@/components/ui/toast';
 import { useToast } from '@/components/ui/use-toast';
+import { Check, X } from 'lucide-react';
 
 export function Toaster() {
   const { toasts } = useToast();
@@ -19,6 +20,16 @@ export function Toaster() {
         return (
           <Toast key={id} {...props}>
             <div className='flex items-center gap-x-2'>
+              {props.variant === 'default' && (
+                <div className='flex-center h-5 w-5 rounded-full bg-[#34D399]'>
+                  <Check size={16} color='rgb(255, 255, 255)' />
+                </div>
+              )}
+              {props.variant === 'destructive' && (
+                <div className='flex-center h-5 w-5 rounded-full bg-[#F87171]'>
+                  <X size={16} color='rgb(255, 255, 255)' />
+                </div>
+              )}
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
                 <ToastDescription>{description}</ToastDescription>
