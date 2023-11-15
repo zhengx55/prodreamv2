1948acf35b5e7d0c76f084b297cb27d433b34c21
zhengx55 <williamzhxin@gmail.com>
2023-11-15 18:37:02 -0500

    scroll fixed #3

--- app/writtingpal/brainstorm/[id]/chat/@chatpanel/page.tsx
+++ app/writtingpal/brainstorm/[id]/chat/@chatpanel/page.tsx
@@ -103,7 +103,7 @@ const ChatPanel = () => {
         animate={{ y: 0, opacity: 1 }}
         exit={{ y: -10, opacity: 0 }}
         transition={{ duration: 0.2 }}
-        className='md:custom-scrollbar relative flex h-[calc(100vh_-var(--top-nav-bar-height))] w-full flex-col items-center px-4 pt-5 md:min-h-[500px] md:overflow-y-auto md:px-10 md:pt-10'
+        className='md:custom-scrollbar relative flex h-[calc(100vh_-var(--top-nav-bar-height))] w-full flex-col items-center px-4 pt-5 md:overflow-y-auto md:px-10 md:pt-10'
       >
         {/* steps */}
         <h2 className='h3-semibold self-start text-primary-200'>
--- components/chat/messages/ActivityMessage.tsx
+++ components/chat/messages/ActivityMessage.tsx
@@ -1,29 +1,16 @@
 'use client';
-import React, { memo, useState } from 'react';
+import React, { memo } from 'react';
 import ChatIconOption from '../ChatIconOption';
 import { messageOptions } from '@/constant';
-import { Button } from '@/components/ui/button';
 
 type Props = {};
 
 const ActivityMessage = (props: Props) => {
-  const [showOptions, setShowOptions] = useState(false);
   return (
     <div className='flex w-[80%] min-w-[485px] flex-col self-start rounded-xl bg-shadow-200 p-4'>
       <p className='small-regular text-black-700'>
         Great! You can click Add Activity to add a new experience
       </p>
-      <div className='flex-start mt-2'>
-        <Button
-          variant={'bold'}
-          size={'sm'}
-          onClick={() => {
-            setShowOptions((prev) => !prev);
-          }}
-        >
-          {showOptions ? 'Hide' : 'Show'} Activity
-        </Button>
-      </div>
 
       <div className='mt-4 flex flex-wrap gap-4'>
         {messageOptions.map((item) => (
