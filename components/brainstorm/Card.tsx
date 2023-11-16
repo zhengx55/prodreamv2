9e234a6ff43a56aaf6b79762766a0a66be5ad47e
zhengx55 <williamzhxin@gmail.com>
2023-11-16 11:55:05 -0500

    brain storm add width

--- components/brainstorm/Card.tsx
+++ components/brainstorm/Card.tsx
@@ -19,10 +19,12 @@ const Card = ({ cardItem }: Props) => {
           router.push(`/writtingpal/brainstorm/${cardItem.id}`);
         }
       }}
-      className='flex h-[200px] w-[420px] shrink-0 cursor-pointer flex-col rounded-lg border border-shadow-border bg-white hover:bg-hover-50 md:p-5'
+      className='flex h-[200px] w-[350px] shrink-0 cursor-pointer flex-col rounded-lg border border-shadow-border bg-white hover:bg-hover-50 md:p-5'
     >
       <h1 className='title-semibold'>{cardItem.name}</h1>
-      <p className='small-regular mt-2 text-shadow'>{cardItem.description}</p>
+      <p className='small-regular mt-2 line-clamp-2 text-shadow'>
+        {cardItem.description}
+      </p>
       <span className='flex-center small-regular mt-auto h-[30px] w-[65px] rounded-xl bg-primary-50 px-2 py-1 text-primary-200'>
         {cardItem.price} Point
       </span>
