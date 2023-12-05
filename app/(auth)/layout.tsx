d4c1994b51273a62d4ecf8f1ec9a71cf844d00d2
zhengx55 <williamzhxin@gmail.com>
2023-12-05 16:22:58 -0500

    rename: file rename

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,11 +2,7 @@ import { AnimatedLogo } from '@/components/root/AnimatedLogo';
 import { Toaster } from '@/components/ui/toaster';
 import { ReactNode } from 'react';
 
-export default function WrittingpalLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className='relative flex h-full w-full flex-col overflow-x-auto bg-auth bg-cover bg-center bg-no-repeat'>
       <div className='mb-5 flex w-full px-5 pt-5 md:mb-0 md:pl-20 md:pt-10'>
