5bd5ff75a6548b33fddfcc39e8f88d08e19e4ec8
zhengx55 <williamzhxin@gmail.com>
2023-12-05 20:23:07 -0500

    revert: bg image auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,20 +1,11 @@
 import { AnimatedLogo } from '@/components/root/AnimatedLogo';
 import { Toaster } from '@/components/ui/toaster';
-import Image from 'next/image';
 import { ReactNode } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='relative flex h-full w-full flex-col overflow-x-auto'>
-      <Image
-        src='/auth.svg'
-        fill
-        alt='auth'
-        priority
-        objectFit='cover'
-        className='z-0'
-      />
-      <div className='z-10 mb-5 flex w-full px-5 pt-5 md:mb-0 md:pl-20 md:pt-10'>
+    <div className='relative flex h-full w-full flex-col overflow-x-auto bg-auth bg-cover bg-center bg-no-repeat'>
+      <div className='mb-5 flex w-full px-5 pt-5 md:mb-0 md:pl-20 md:pt-10'>
         <AnimatedLogo pathFill='#fff' show />
         <Toaster />
       </div>
