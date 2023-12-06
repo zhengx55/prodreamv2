6fd14890f4355f0aab9556daf502254927da8891
zhengx55 <williamzhxin@gmail.com>
2023-12-05 20:13:32 -0500

    chage: background-image to next-image

--- components/auth/Panel.tsx
+++ components/auth/Panel.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react';
 
 const Panel = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex w-[90%] flex-col rounded-[32px] bg-white p-5 md:w-[540px] md:p-10 '>
+    <div className='z-10 flex w-[90%] flex-col rounded-[32px] bg-white p-5 md:w-[540px] md:p-10 '>
       {children}
     </div>
   );
