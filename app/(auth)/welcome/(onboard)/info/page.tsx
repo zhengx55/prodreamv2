844dd4f8ac8b01eda59e8b7ada6a550a1c2f639c
zhengx55 <williamzhxin@gmail.com>
2023-12-08 18:17:54 -0500

    features: add welcom routes

--- app/(auth)/welcome/(onboard)/info/page.tsx
+++ app/(auth)/welcome/(onboard)/info/page.tsx
@@ -1,17 +1,42 @@
 import Spacer from '@/components/root/Spacer';
-import { buttonVariants } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { cn } from '@/lib/utils';
+import { profileResetName } from '@/query/api';
 import dynamic from 'next/dynamic';
+import { cookies } from 'next/headers';
 import Image from 'next/image';
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 const AnimatedLayout = dynamic(
   () => import('@/components/welcome/AnimatedLayout'),
   { ssr: false }
 );
 
-export default function Page() {
+export default async function Page() {
+  const cookieStore = cookies();
+  if (!cookieStore.get('token')) {
+    redirect('/signup');
+  }
+  const token = cookieStore.get('token')?.value;
+  const handleSubmit = async (formData: FormData) => {
+    'use server';
+    const firstname = formData.get('firstname');
+    const lastname = formData.get('lastname');
+    if (!firstname || !lastname) {
+      return;
+    }
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}user/name?last_name=${lastname}&first_name=${firstname}`,
+      {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const data = await res.json();
+    if (data.code === 0) redirect('/welcome/features');
+  };
   return (
     <AnimatedLayout>
       <div className='flex h-max gap-x-8'>
@@ -40,35 +65,37 @@ export default function Page() {
             To get started, may I know your name?
           </p>
           <Spacer y='32' />
-          <div className='flex gap-x-4'>
-            <div className='flex w-1/2 flex-col gap-y-2'>
-              <label className='base-semibold' htmlFor='firstname'>
-                First Name/Preferred Name
-              </label>
-              <Input
-                id='firstname'
-                className=' w-full border-shadow-border bg-transparent'
-              />
+          <form action={handleSubmit} className='flex flex-1 flex-col'>
+            <div className='flex gap-x-4'>
+              <div className='flex w-1/2 flex-col gap-y-4'>
+                <label className='base-semibold' htmlFor='firstname'>
+                  First Name/Preferred Name
+                </label>
+                <Input
+                  id='firstname'
+                  name='firstname'
+                  type='text'
+                  className=' w-full border-shadow-border bg-transparent'
+                  required
+                />
+              </div>
+              <div className='flex w-1/2 flex-col gap-y-4'>
+                <label className='base-semibold' htmlFor='lastname'>
+                  Last Name
+                </label>
+                <Input
+                  id='lastname'
+                  name='lastname'
+                  type='text'
+                  className=' w-full border-shadow-border bg-transparent'
+                  required
+                />
+              </div>
             </div>
-            <div className='flex w-1/2 flex-col gap-y-2'>
-              <label className='base-semibold' htmlFor='lastname'>
-                Last Name
-              </label>
-              <Input
-                id='lastname'
-                className=' w-full border-shadow-border bg-transparent'
-              />
-            </div>
-          </div>
-          <Link
-            href={'/welcome/preselect'}
-            className={cn(
-              buttonVariants({ variant: 'default' }),
-              'mt-auto self-end'
-            )}
-          >
-            Next
-          </Link>
+            <Button type='submit' className='mt-auto self-end'>
+              Next
+            </Button>
+          </form>
         </div>
       </div>
     </AnimatedLayout>
