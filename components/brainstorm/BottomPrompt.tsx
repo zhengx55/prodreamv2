dfe6bfe634f2a4748bf29f3e63565ebd08524120
zhengx55 <williamzhxin@gmail.com>
2023-12-11 12:43:06 -0500

    remove: unused components

--- components/brainstorm/BottomPrompt.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const BottomPrompt = (props: Props) => {
-  return <div className='absolute'>BottomPrompt</div>;
-};
-
-export default BottomPrompt;
