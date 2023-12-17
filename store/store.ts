07bc378325746683f3057a4ad0881f944a7c7435
zhengx55 <williamzhxin@gmail.com>
2023-12-16 21:31:29 -0500

    remove: remove old context provider

--- store/store.ts
+++ store/store.ts
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import resumeReducer from './reducers/resumeSlice';
 import userReducer from './reducers/userSlice';
 import usageReducer from './reducers/usageSlice';
-import essayReducer from './reducers/essaySlice';
 export const store = configureStore({
   reducer: {
     resume: resumeReducer,
     user: userReducer,
     usage: usageReducer,
-    essay: essayReducer,
   },
 });
 
