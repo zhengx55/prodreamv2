691bd9cb26551182cc9f58a75fd2c342e8680077
zhengx55 <williamzhxin@gmail.com>
2024-01-07 23:58:38 -0500

    change: dependency change

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
-const cssnano = [
-  'cssnano',
-  {
-    preset: 'advanced',
-    discardComments: { removeAll: true },
-  },
-];
 module.exports = {
   plugins: {
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? [cssnano] : []),
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          cssnano: {
+            preset: 'advanced',
+            discardComments: { removeAll: true },
+          },
+        }
+      : {}),
   },
 };
