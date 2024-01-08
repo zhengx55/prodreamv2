f7dda96483bdd7edaeb8764b6f85de4c5aa6afb9
zhengx55 <williamzhxin@gmail.com>
2024-01-07 23:55:28 -0500

    change: dependency change

--- app/writtingpal/polish/[id]/page.tsx
+++ app/writtingpal/polish/[id]/page.tsx
@@ -1,7 +1,7 @@
 import EssayPanel from '@/components/polish/EssayPanel';
 import Rightbar from '@/components/polish/rightbar';
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: string } }) {
   return (
     <main className='relative flex h-[calc(100%_-var(--top-nav-bar-height))] w-full justify-center overflow-hidden pr-[240px]'>
       <EssayPanel id={params.id} />
