593e42d7905fd45c16ad76ef34d70d7b93a2a369
zhengx55 <williamzhxin@gmail.com>
2024-01-13 07:13:14 -0500

    archived: 删除无用store 以及 slice

--- components/root/GlobalInfoProvider.tsx
+++ components/root/GlobalInfoProvider.tsx
@@ -1,8 +1,7 @@
 'use client';
-import { initialUsage } from '@/constant';
 import useMount from '@/hooks/useMount';
 import { getUserInfo, refreshUserSession } from '@/query/api';
-import { useUsage, useUserInfo } from '@/zustand/store';
+import { useUserInfo } from '@/zustand/store';
 import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
 import { useCookies } from 'react-cookie';
@@ -10,7 +9,6 @@ import { useCookies } from 'react-cookie';
 const GlobalInfoProvider = ({ children }: { children: ReactNode }) => {
   const updateUserInfo = useUserInfo((state) => state.setUser);
   const [_cookies, setCookie] = useCookies(['token']);
-  const updateUsage = useUsage((state) => state.updateUsage);
   useMount(() => {
     async function refreshUserInfo() {
       try {
@@ -21,8 +19,6 @@ const GlobalInfoProvider = ({ children }: { children: ReactNode }) => {
          * 用于检查是否是第一次登录 或是 第一次使用某些功能
          **/
         updateUserInfo(data);
-        if (user_usage) updateUsage(user_usage);
-        else updateUsage(initialUsage);
         setCookie('token', data.access_token, {
           path: '/',
           maxAge: 604800,
--- zustand/store.ts
+++ zustand/store.ts
@@ -1,20 +1,19 @@
 import { create, useStore } from 'zustand';
 import { AIEditiorStore, useAIEditorStore } from './slice/ai-editor';
-import { UsageStore, useUsageStore } from './slice/usage';
 import useUserStore, { UserStore } from './slice/user-info';
 
 const useRootStore = create<
   AIEditiorStore &
     // BrainstormStore &
     // ActivityListStore &
-    UsageStore &
+    // UsageStore &
     UserStore
   // ResumeStore
 >((...a) => ({
   ...useAIEditorStore(...a),
   // ...useBrainStorm(...a),
   // ...useActListStore(...a),
-  ...useUsageStore(...a),
+  // ...useUsageStore(...a),
   ...useUserStore(...a),
   // ...useResumeStore(...a),
 }));
@@ -26,9 +25,10 @@ export function useAIEditor<T>(selector?: (state: AIEditiorStore) => T) {
 // export function useResume<T>(selector?: (state: ResumeStore) => T) {
 //   return useStore(useRootStore, selector!);
 // }
-export function useUsage<T>(selector?: (state: UsageStore) => T) {
-  return useStore(useRootStore, selector!);
-}
+
+// export function useUsage<T>(selector?: (state: UsageStore) => T) {
+//   return useStore(useRootStore, selector!);
+// }
 
 export function useUserInfo<T>(selector?: (state: UserStore) => T) {
   return useStore(useRootStore, selector!);
