2d5c867b9d7f6e646d4edf3d901a3db05e3e3ed1
zhengx55 <williamzhxin@gmail.com>
2024-01-23 00:32:26 -0500

    feature:citation rightbar #3 website & journal

--- components/editor/rightbar/citation/form/Website.tsx
+++ components/editor/rightbar/citation/form/Website.tsx
@@ -5,7 +5,7 @@ import { contributorAnimation } from '@/constant';
 import { IWebsiteCitation } from '@/types';
 import useAiEditor from '@/zustand/store';
 import { AnimatePresence, m } from 'framer-motion';
-import { Trash2 } from 'lucide-react';
+import { PlusCircle, Trash2 } from 'lucide-react';
 import { useFieldArray, useForm } from 'react-hook-form';
 
 type Props = {};
@@ -45,13 +45,12 @@ const WebsiteForm = (props: Props) => {
       />
       <Spacer y='48' />
       <h1 className='base-semibold'>Contributors</h1>
-      <Spacer y='16' />
       <AnimatePresence>
         <div className='flex flex-col gap-y-2'>
           {fields.map((contributor, index) => (
             <m.div
               key={contributor.id}
-              className='flex items-center gap-x-2'
+              className='flex items-center gap-x-2 first:mt-4'
               initial='initial'
               animate='animate'
               exit='exit'
@@ -101,19 +100,18 @@ const WebsiteForm = (props: Props) => {
           ))}
         </div>
       </AnimatePresence>
-      <button type='button' onClick={appendContributor}>
-        Add Contributor
-      </button>
+      <Spacer y='10' />
+      <Button
+        variant={'ghost'}
+        className='gap-x-1 px-0'
+        type='button'
+        onClick={appendContributor}
+      >
+        <PlusCircle className='fill-doc-primary text-white' size={22} />
+        <p className='text-doc-primary'> Add Contributor</p>
+      </Button>
       <Spacer y='48' />
       <h1 className='base-semibold'>Online publication info</h1>
-      <Spacer y='16' />
-      <label htmlFor='document_id'>Document ID</label>
-      <Input
-        type='text'
-        id='document_id'
-        className='focus-visible:ring-0'
-        {...register('document_id')}
-      />
       <Spacer y='16' />
       <label htmlFor='publisher'>Publisher</label>
       <Input
@@ -132,31 +130,30 @@ const WebsiteForm = (props: Props) => {
       />
       <Spacer y='16' />
       <h2>Date accessed</h2>
-      <Spacer y='16' />
       <div className='flex gap-x-2'>
         <div className='flex flex-col'>
-          <label htmlFor='access_date.day'>Day</label>
           <Input
             type='text'
             id='access_date.day'
+            placeholder='Day'
             className='focus-visible:ring-0'
             {...register('access_date.day')}
           />
         </div>
         <div className='flex flex-col'>
-          <label htmlFor='access_date.month'>Month</label>
           <Input
             id='access_date.month'
+            placeholder='Month'
             type='text'
             className='focus-visible:ring-0'
             {...register('access_date.month')}
           />
         </div>
         <div className='flex flex-col'>
-          <label htmlFor='access_date.year'>Year</label>
           <Input
             id='access_date.year'
             type='text'
+            placeholder='Year'
             className='focus-visible:ring-0'
             {...register('access_date.year')}
           />
@@ -165,7 +162,7 @@ const WebsiteForm = (props: Props) => {
       <Spacer y='48' />
       <h1 className='base-semibold'>More options</h1>
       <Spacer y='16' />
-      <div className='flex w-full justify-end gap-x-2 border-t border-shadow-border py-2'>
+      <div className='flex w-full justify-end gap-x-2 border-t border-shadow-border py-3'>
         <Button
           className='rounded border border-doc-primary text-doc-primary'
           variant={'ghost'}
