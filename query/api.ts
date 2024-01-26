f608d1690860bb059f0c8dd2c3e68e598eed3162
lifang <619162565@qq.com>
2024-01-26 11:02:05 +0800

    mod:类型问题3

--- query/api.ts
+++ query/api.ts
@@ -126,7 +126,7 @@ export async function userLogin(loginParam: {
 export async function userSignUp(signUpParam: ISigunUpRequest) {
   try {
     let flag:boolean = false 
-    flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+    flag = ()=> navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
     const formdata = new FormData();
     formdata.append('first_name', signUpParam.first_name);
     formdata.append('last_name', signUpParam.last_name);
