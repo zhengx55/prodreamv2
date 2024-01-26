4bbd3874cb7160c8adf90f68b2ab096d5700f2f8
lifang <619162565@qq.com>
2024-01-26 22:34:43 +0800

    mod:删除参数2

--- app/(auth)/welcome/page.tsx
+++ app/(auth)/welcome/page.tsx
@@ -13,18 +13,11 @@ export default async function Page() {
     redirect('/login');
   }
   const token = cookieStore.get('token')?.value;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}refresh`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const data = (await res.json()).data;
 
   return (
     <AnimatedLayout>
       <div className='flex flex-col text-white'>
-        <Options firstname={data.first_name} />
+        <Options />
       </div>
     </AnimatedLayout>
   );
