917d490e25ce4104ba68c898f050341deb2cabbb
zhengx55 <williamzhxin@gmail.com>
2024-02-01 18:10:08 -0500

    fixed: code adjust

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,6 @@
-'use client';
-import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-  const router = useRouter();
   return (
     <div className='relative flex h-full w-full overflow-auto sm:flex-row'>
       {children}
