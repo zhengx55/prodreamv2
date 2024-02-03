bb58671c91f9600a4064f3529ca8ff5098d61244
zhengx55 <williamzhxin@gmail.com>
2024-02-02 20:37:53 -0500

    feature: 新版链路#4

--- components/polish/Editor.tsx
+++ components/polish/Editor.tsx
@@ -9,11 +9,14 @@ import useAiEditor, { useUserInfo } from '@/zustand/store';
 import { useMutation } from '@tanstack/react-query';
 import { Editor as EditorType, useEditor } from '@tiptap/react';
 import useLocalStorage from 'beautiful-react-hooks/useLocalStorage';
+import { AnimatePresence } from 'framer-motion';
 import { useParams } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
 import TableOfContents from '../editor/table-of-contents/TableOfContents';
+import LazyMotionProvider from '../root/LazyMotionProvider';
 import EditorContent from './EditorContent';
+import Guidence from './guide/Guidence';
 
 const Editor = ({ essay_content }: { essay_content: string }) => {
   const { id }: { id: string } = useParams();
@@ -106,13 +109,13 @@ const Editor = ({ essay_content }: { essay_content: string }) => {
     <section className='flex w-full flex-col'>
       <div className='relative flex h-[calc(100%_-40px)] w-full'>
         <TableOfContents editor={editor} />
-        {/* <LazyMotionProvider>
+        <LazyMotionProvider>
           <AnimatePresence initial={false}>
             {showGuidence && (
               <Guidence editor={editor} close={memoHideGuidence} />
             )}
           </AnimatePresence>
-        </LazyMotionProvider> */}
+        </LazyMotionProvider>
         <EditorContent editor={editor} />
       </div>
       {showBottomBar && (
