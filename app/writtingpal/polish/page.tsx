3bbdce6897e3d714f106fad291031ac241126e0c
zhengx55 <williamzhxin@gmail.com>
2024-02-03 22:02:36 -0500

    feature: 新版链路#8

--- app/writtingpal/polish/page.tsx
+++ app/writtingpal/polish/page.tsx
@@ -1,5 +1,16 @@
 import DocHistory from '@/components/polish/history';
+import Spacer from '@/components/root/Spacer';
+import dynamic from 'next/dynamic';
+const Search = dynamic(() => import('@/components/polish/history/Search'));
 
 export default async function Page() {
-  return <DocHistory />;
+  return (
+    <main className='relative flex h-full w-full flex-col items-center overflow-y-auto'>
+      <Spacer y='75' />
+      <Search />
+      <Spacer y='48' />
+      <DocHistory />
+      <Spacer y='14' />
+    </main>
+  );
 }
--- components/polish/history/Search.tsx
+++ components/polish/history/Search.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { UploadGard } from '@/components/root/SvgComponents';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
