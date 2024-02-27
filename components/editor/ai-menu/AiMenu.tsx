0c653a0a51373d7ec2c3806a9e15b5aaad05c02b
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-02-26 20:48:21 -0500

    feature: ai-menu

--- components/editor/ai-menu/AiMenu.tsx
+++ components/editor/ai-menu/AiMenu.tsx
@@ -1,38 +1,28 @@
 import LoadingDot from '@/components/root/LoadingDot';
 import Spacer from '@/components/root/Spacer';
 import { Copilot } from '@/components/root/SvgComponents';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
 import { Surface } from '@/components/ui/surface';
 import useClickOutside from '@/hooks/useClickOutside';
 import useScrollIntoView from '@/hooks/useScrollIntoView';
 import { getSelectedText } from '@/lib/tiptap/utils';
-import { ButtonTrack, ask, copilot } from '@/query/api';
+import { ButtonTrack } from '@/query/api';
 import {
   useMembershipInfo,
   useMutateTrackInfo,
   useUserTrackInfo,
 } from '@/query/query';
 import useAiEditor from '@/zustand/store';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { Editor } from '@tiptap/react';
-import { AlertTriangle, ChevronRight } from 'lucide-react';
-import dynamic from 'next/dynamic';
-import {
-  ChangeEvent,
-  Fragment,
-  KeyboardEvent,
-  cloneElement,
-  memo,
-  useRef,
-  useState,
-} from 'react';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { Fragment, cloneElement, memo, useCallback, useRef } from 'react';
 import { v4 } from 'uuid';
 import { useEditorCommand } from '../hooks/useEditorCommand';
+import CustomPrompt from './CustomPrompt';
+import RemainUsages from './RemainUsages';
+import StreamText from './StreamText';
 import { useAiOptions } from './hooks/useAiOptions';
-
-const Typed = dynamic(() => import('react-typed'), { ssr: false });
+import useAiResponse from './hooks/useAiResponse';
 
 type Props = { editor: Editor };
 const AiMenu = ({ editor }: Props) => {
@@ -41,105 +31,30 @@ const AiMenu = ({ editor }: Props) => {
   const { data: track } = useUserTrackInfo();
   const { data: usage } = useMembershipInfo();
   const copilotRect = useAiEditor((state) => state.copilotRect);
-  const queryClient = useQueryClient();
   const updateCopilotMenu = useAiEditor((state) => state.updateCopilotMenu);
-  const updatePaymentModal = useAiEditor((state) => state.updatePaymentModal);
   const promptRef = useRef<HTMLInputElement>(null);
-  const [hoverItem, setHoverItem] = useState<string | null>(null);
-  const [generating, setGenerating] = useState(false);
-  const [aiResult, setAiResult] = useState('');
-  const [istTyping, setIsTyping] = useState(false);
-  const elRef = useRef<HTMLDivElement>(null);
+
   const tool = useRef<string | null>(null);
   const { replaceText, insertNext } = useEditorCommand(editor);
-  const ref = useScrollIntoView();
+  const elRef = useScrollIntoView();
 
-  const hasAiResult = aiResult !== '';
+  const {
+    hoverItem,
+    setHoverItem,
+    generating,
+    handleAsk,
+    handleCopilot,
+    aiResult,
+    currentResult,
+    setCurrentResult,
+    showTyping,
+    toogleTyping,
+  } = useAiResponse(tool);
+
+  const hasAiResult = aiResult.length > 0;
   useClickOutside(elRef, () => {
     updateCopilotMenu(false);
   });
-
-  const handlePromptChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (value.trim() !== '') {
-      setIsTyping(true);
-    } else {
-      setIsTyping(false);
-    }
-  };
-
-  const { mutateAsync: handleCopilot } = useMutation({
-    mutationFn: (params: { tool: string; text: string }) => copilot(params),
-    onMutate: () => {
-      setGenerating(true);
-    },
-    onSuccess: async (data: ReadableStream, variables) => {
-      queryClient.invalidateQueries({ queryKey: ['membership'] });
-      tool.current = variables.tool;
-      const reader = data.pipeThrough(new TextDecoderStream()).getReader();
-      while (true) {
-        const { value, done } = await reader.read();
-        if (done) {
-          setHoverItem('copilot-operation-01');
-          break;
-        }
-        handleStreamData(value);
-      }
-    },
-    onSettled: () => {
-      setGenerating(false);
-    },
-
-    onError: async (error) => {
-      const toast = (await import('sonner')).toast;
-      toast.error(error.message);
-    },
-  });
-
-  const { mutateAsync: handleAsk } = useMutation({
-    mutationFn: (params: { instruction: string; text: string }) => ask(params),
-    onMutate: () => {
-      setGenerating(true);
-    },
-    onSuccess: async (data: ReadableStream) => {
-      queryClient.invalidateQueries({ queryKey: ['membership'] });
-      const reader = data.pipeThrough(new TextDecoderStream()).getReader();
-      while (true) {
-        const { value, done } = await reader.read();
-        if (done) {
-          setHoverItem('copilot-operation-01');
-          break;
-        }
-        handleStreamData(value);
-      }
-    },
-    onSettled: () => {
-      setGenerating(false);
-    },
-
-    onError: async (error) => {
-      const toast = (await import('sonner')).toast;
-      toast.error(error.message);
-    },
-  });
-
-  const handleStreamData = (value: string | undefined) => {
-    if (!value) return;
-    const lines = value.split('\n');
-    const dataLines = lines.filter(
-      (line, index) =>
-        line.startsWith('data:') &&
-        lines.at(index - 1)?.startsWith('event: data')
-    );
-    const eventData = dataLines.map((line) =>
-      JSON.parse(line.slice('data:'.length))
-    );
-    let result = '';
-    eventData.forEach((word) => {
-      result += word;
-    });
-    setAiResult((prev) => (prev += result));
-  };
 
   const handleEditTools = async (tool: string) => {
     const toast = (await import('sonner')).toast;
@@ -158,7 +73,7 @@ const AiMenu = ({ editor }: Props) => {
     await handleCopilot({ tool, text: selectedText });
   };
 
-  const handleCustomPrompt = async () => {
+  const handleCustomPrompt = useCallback(async () => {
     const toast = (await import('sonner')).toast;
     const selectedText = getSelectedText(editor);
     const words = selectedText.match(/\b\w+\b/g);
@@ -167,24 +82,13 @@ const AiMenu = ({ editor }: Props) => {
     }
     if (promptRef.current && !promptRef.current.value.trim())
       return toast.error('please enter a custom prompt');
-    if (!track?.ai_copilot_task) {
-      await updateTrack({
-        field: 'ai_copilot_task',
-        data: true,
-      });
-      await ButtonTrack('ai_copilot_task_completed');
-    }
+
     await handleAsk({
       instruction: promptRef.current?.value!,
       text: selectedText,
     });
-  };
-
-  const handleKeyEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.code === 'Enter') {
-      handleCustomPrompt();
-    }
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editor]);
 
   const handleDiscard = () => {
     updateCopilotMenu(false);
@@ -192,6 +96,7 @@ const AiMenu = ({ editor }: Props) => {
 
   const handleRegenerate = async () => {
     const selectedText = getSelectedText(editor);
+    setCurrentResult((prev) => prev + 1);
     if (!tool.current) return;
     await handleCopilot({ tool: tool.current, text: selectedText });
   };
@@ -199,14 +104,14 @@ const AiMenu = ({ editor }: Props) => {
   const handleReplace = () => {
     const { selection } = editor.state;
     const { from, to } = selection;
-    replaceText(from, to, aiResult);
+    replaceText(from, to, aiResult[currentResult]);
     updateCopilotMenu(false);
   };
 
   const handleInsert = () => {
     const { selection } = editor.state;
     const { to } = selection;
-    insertNext(to, aiResult);
+    insertNext(to, aiResult[currentResult]);
     updateCopilotMenu(false);
   };
 
@@ -232,44 +137,58 @@ const AiMenu = ({ editor }: Props) => {
   if (!copilotRect) return null;
   return (
     <section
-      ref={ref}
       style={{ top: `${copilotRect - 54}px` }}
       className='absolute -left-12 z-20 flex w-full justify-center overflow-visible '
     >
       <div
-        ref={elRef}
+        ref={elRef as React.RefObject<HTMLDivElement>}
         className='relative flex w-[600px] flex-col bg-transparent'
       >
         {!generating ? (
           hasAiResult ? (
-            <div className='flex min-h-12 w-full items-center rounded-t border border-shadow-border bg-white p-2 shadow-lg'>
-              <Typed
-                strings={[aiResult]}
-                className='small-regular px-2'
-                typeSpeed={2}
-              />
+            <div className='flex min-h-12 w-full flex-col justify-center rounded-t border border-shadow-border bg-white p-2 shadow-lg'>
+              {showTyping ? (
+                <p className='small-regular px-2'>
+                  <StreamText
+                    toogleTyping={toogleTyping}
+                    generatedResult={aiResult[currentResult]}
+                  />
+                </p>
+              ) : (
+                <p className='small-regular px-2'>{aiResult[currentResult]}</p>
+              )}
+              {!showTyping && (
+                <div className='flex w-full items-center justify-end gap-x-0.5'>
+                  <ChevronLeft
+                    className='cursor-pointer text-doc-font'
+                    size={18}
+                    onClick={() =>
+                      setCurrentResult((prev) => (prev === 0 ? 0 : prev - 1))
+                    }
+                  />
+                  <p className='small-regular text-doc-font'>
+                    {currentResult + 1} of {aiResult.length}
+                  </p>
+                  <ChevronRight
+                    className='cursor-pointer text-doc-font'
+                    size={18}
+                    onClick={() =>
+                      setCurrentResult((prev) =>
+                        prev === aiResult.length - 1
+                          ? aiResult.length - 1
+                          : prev + 1
+                      )
+                    }
+                  />
+                </div>
+              )}
             </div>
           ) : (
-            <div className='flex-between h-12 w-full gap-x-2 rounded-t border border-shadow-border bg-white p-2 shadow-lg'>
-              <Copilot size='24' />
-              <Input
-                type='text'
-                ref={promptRef}
-                autoFocus
-                onChange={handlePromptChange}
-                onKeyDown={handleKeyEnter}
-                id='ai-prompt'
-                className='small-regular h-8 border-none px-0 py-0 shadow-none focus-visible:right-0 focus-visible:ring-0'
-                placeholder='Ask Al to edit or generate...'
-              />
-              <Button
-                onClick={handleCustomPrompt}
-                disabled={!istTyping}
-                className='h-7 rounded bg-doc-primary disabled:bg-doc-shadow'
-              >
-                Enter
-              </Button>
-            </div>
+            <CustomPrompt
+              ref={promptRef}
+              editor={editor}
+              submit={handleCustomPrompt}
+            />
           )
         ) : (
           <div className='flex h-12 w-full items-center gap-x-2 rounded-t border border-shadow-border bg-white p-2 shadow-lg'>
@@ -279,27 +198,7 @@ const AiMenu = ({ editor }: Props) => {
             </p>
           </div>
         )}
-        {usage?.subscription === 'basic' && (
-          <div className='flex-between w-[600px] rounded-b bg-border-50 px-2 py-1'>
-            <div className='flex items-center gap-x-2'>
-              <AlertTriangle className='text-shadow' size={15} />
-              <p className='subtle-regular text-shadow'>
-                {usage?.free_times_detail.Copilot}/20 weekly AI prompts
-                used;&nbsp;
-                <Button
-                  onClick={() => {
-                    updatePaymentModal(true);
-                  }}
-                  role='button'
-                  variant={'ghost'}
-                  className='subtle-regular h-max w-max cursor-pointer bg-transparent p-0 text-doc-primary'
-                >
-                  Go unlimited
-                </Button>
-              </p>
-            </div>
-          </div>
-        )}
+        {usage?.subscription === 'basic' && <RemainUsages />}
         <Spacer y='5' />
         {generating ? null : (
           <Surface className='w-[256px] rounded py-2' withBorder>
@@ -375,7 +274,7 @@ const AiMenu = ({ editor }: Props) => {
                     <div
                       className={` ${
                         hoverItem === item.id ? 'bg-doc-secondary' : ''
-                      } group flex cursor-pointer items-center justify-between rounded px-2 py-1.5`}
+                      } ${showTyping && 'pointer-events-none'} group flex cursor-pointer items-center justify-between rounded px-2 py-1.5`}
                       key={item.id}
                       onMouseEnter={() => setHoverItem(item.id)}
                       onMouseLeave={() => setHoverItem(null)}
