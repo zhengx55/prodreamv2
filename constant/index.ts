d5d102291636df86e68efe9270433160e600ccba
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-03-07 19:29:01 -0500

    feature: Add membership info to GenerateSub component

--- constant/index.ts
+++ constant/index.ts
@@ -47,10 +47,10 @@ export const GenerateOptions = [
     id: 'generation-04',
     title: 'Generate Outline',
     submenu: [
-      { id: 'outline-04', label: 'general' },
       { id: 'outline-01', label: 'argumentative' },
       { id: 'outline-02', label: 'analytical' },
       { id: 'outline-03', label: 'scientific' },
+      { id: 'outline-04', label: 'general' },
     ],
   },
 ];
