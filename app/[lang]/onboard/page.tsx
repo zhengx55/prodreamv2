ac95f948cbec90d86a142a0bbf14bf8a234e97ee
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-03-15 02:08:55 -0400

    refactor: Refactor onboard page and extract InfoForm component

--- app/[lang]/onboard/page.tsx
+++ app/[lang]/onboard/page.tsx
@@ -1,11 +1,8 @@
+import InfoForm from '@/components/onboard/InfoForm';
 import Progress from '@/components/onboard/Progress';
 import Spacer from '@/components/root/Spacer';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import type { Locale } from '@/i18n-config';
 import { getDictionary } from '@/lib/get-dictionary';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export default async function Page({
   params: { lang },
@@ -15,38 +12,6 @@ export default async function Page({
   searchParams: { name?: string };
 }) {
   const dict = await getDictionary(lang);
-  const token = cookies().get('token')?.value;
-  async function updateInfo(formData: FormData) {
-    'use server';
-    const nameForm = new FormData();
-    const codeForm = new FormData();
-    nameForm.append('first_name', formData.get('first_name') as string);
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}v1/user/name`,
-      {
-        method: 'PUT',
-        body: formData,
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    if (formData.get('code')) {
-      codeForm.append('referral_code', formData.get('code') as string);
-      const code_res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}v1/user/referral_code`,
-        {
-          method: 'PUT',
-          body: codeForm,
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      console.log(await code_res.json());
-    }
-    redirect(`/${lang}/onboard/education`);
-  }
 
   return (
     <div className='flex w-full flex-col items-center pt-20'>
@@ -57,36 +22,7 @@ export default async function Page({
         </p>
       </div>
       <Spacer y='150' />
-      <form action={updateInfo} className='flex w-[500px] flex-col'>
-        <Input
-          id='name'
-          required
-          defaultValue={name ?? ''}
-          name='first_name'
-          aria-placeholder='name'
-          className='title-regular h-14'
-          placeholder={dict.Onboard.FormName}
-        />
-        <Spacer y='20' />
-        <Input
-          id='code'
-          name='code'
-          aria-placeholder='code'
-          className='title-regular h-14'
-          placeholder={dict.Onboard.FormCode}
-        />
-        <Spacer y='10' />
-        <p className='subtle-regular text-neutral-400'>{dict.Onboard.Option}</p>
-        <Spacer y='80' />
-
-        <Button
-          role='button'
-          type='submit'
-          className='title-medium h-max w-full rounded-lg py-4'
-        >
-          {dict.Onboard.Button}
-        </Button>
-      </form>
+      <InfoForm name={name} dict={dict} lang={lang} />
       <Spacer y='110' />
       <Progress />
     </div>
--- components/onboard/InfoForm.tsx
+++ components/onboard/InfoForm.tsx
@@ -1,6 +1,74 @@
 'use client';
-type Props = {};
-const InfoForm = (props: Props) => {
-  return <div>InfoForm</div>;
+
+import { Locale } from '@/i18n-config';
+import { getDictionary } from '@/lib/get-dictionary';
+import { Loader2 } from 'lucide-react';
+import { useState } from 'react';
+import { useFormStatus } from 'react-dom';
+import Spacer from '../root/Spacer';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { setOnboardNameAndCode } from './_action';
+
+type Props = {
+  dict: Awaited<ReturnType<typeof getDictionary>>;
+  name?: string;
+  lang: Locale;
 };
+const InfoForm = ({ dict, name, lang }: Props) => {
+  const [errorMessage, setErrorMessage] = useState('');
+  async function updateInfo(formData: FormData) {
+    const result = await setOnboardNameAndCode(formData, lang);
+    if (result.error) {
+      setErrorMessage(result.error);
+      return;
+    }
+  }
+  return (
+    <form action={updateInfo} className='flex w-[500px] flex-col'>
+      <Input
+        id='name'
+        required
+        defaultValue={name ?? ''}
+        name='first_name'
+        aria-placeholder='name'
+        className='title-regular h-14'
+        placeholder={dict.Onboard.FormName}
+      />
+      <Spacer y='20' />
+      <Input
+        id='code'
+        name='code'
+        aria-placeholder='code'
+        className={`title-regular h-14 ${errorMessage ? ' border-red-400 bg-red-50' : ''}`}
+        placeholder={dict.Onboard.FormCode}
+      />
+      <div className='flex w-full justify-end'>
+        <p className='small-medium text-red-400'>{errorMessage}</p>
+      </div>
+      <Spacer y='10' />
+      <p className='subtle-regular text-neutral-400'>{dict.Onboard.Option}</p>
+      <Spacer y='80' />
+      <SubmitBtn label={dict.Onboard.Button} />
+    </form>
+  );
+};
+
+const SubmitBtn = ({ label }: { label: string }) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      aria-disabled={pending}
+      disabled={pending}
+      role='button'
+      type='submit'
+      className='title-medium h-max w-full rounded-lg py-4'
+    >
+      {pending && <Loader2 className='animate-spin' size={22} />}
+      {label}
+    </Button>
+  );
+};
+
 export default InfoForm;
