4ce62d3ca2fae1c5816fd5ba8d6796cfe3f40c1e
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-03-15 22:44:41 -0400

    Add PromoCode component and update Membership component

---
+++ components/editor/modal/PromoCode.tsx
@@ -0,0 +1,52 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { ReactNode, useState } from 'react';
+type Props = { children: ReactNode };
+const PromoCode = ({ children }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>{children}</DialogTrigger>
+      <DialogContent
+        onPointerDownOutside={(e) => {
+          e.preventDefault();
+        }}
+        className='rounded-md shadow-md md:w-[540px]'
+      >
+        <DialogHeader>
+          <h2 className='text-xl font-medium leading-7 text-zinc-600'>
+            Promo Code
+          </h2>
+        </DialogHeader>
+        <Input
+          placeholder='enter your code...'
+          id='promo-code'
+          aria-label='promo-code'
+        />
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button
+              role='button'
+              variant={'ghost'}
+              className='border border-neutral-400 text-zinc-400'
+            >
+              Cancle
+            </Button>
+          </DialogClose>
+          <Button role='button'>Redeem</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default PromoCode;
