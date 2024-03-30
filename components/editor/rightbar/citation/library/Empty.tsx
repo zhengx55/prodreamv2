554f38f9601fc19a45d0f6de073de33d0f04dcd0
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-03-29 20:24:50 -0400

    Refactor citation library UI

--- components/editor/rightbar/citation/library/Empty.tsx
+++ components/editor/rightbar/citation/library/Empty.tsx
@@ -1,11 +1,12 @@
+import Spacer from '@/components/root/Spacer';
 import { Button } from '@/components/ui/button';
-import { useMutateTrackInfo } from '@/query/query';
+import { useAIEditor } from '@/zustand/store';
 import { AnimatePresence, m } from 'framer-motion';
 import Image from 'next/image';
 import { memo } from 'react';
 
 const Empty = () => {
-  const { mutateAsync: updateTrack } = useMutateTrackInfo();
+  const updateRightbarTab = useAIEditor((state) => state.updateRightbarTab);
   return (
     <AnimatePresence>
       <m.div
@@ -21,21 +22,18 @@ const Empty = () => {
           height={200}
           className='h-auto w-3/5'
         />
-        <p className='small-regular text-center text-neutral-400'>
-          Your library is currently empty. Try search for citations 😊
+        <p className='small-regular text-center text-zinc-600'>
+          Your library is currently empty.
+          <br /> Try search for citations 😊
         </p>
+        <Spacer y='14' />
         <Button
           role='button'
-          onClick={async () => {
-            await updateTrack({
-              field: 'citation_empty_check',
-              data: true,
-            });
-          }}
+          onClick={() => updateRightbarTab(3)}
           className='h-max w-full border border-violet-500 py-1 text-violet-500'
           variant={'ghost'}
         >
-          Got it
+          Add Citation
         </Button>
       </m.div>
     </AnimatePresence>
