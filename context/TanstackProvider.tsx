3bdab7e3a8f3d4a49ec318ef802eae245c6fecea
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-04-01 16:39:48 -0400

    Add default options for queryClient

--- context/TanstackProvider.tsx
+++ context/TanstackProvider.tsx
@@ -8,6 +8,13 @@ import { ReactNode } from 'react';
 import { toast } from 'sonner';
 
 const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+      retry: 3,
+      refetchOnMount: true,
+    },
+  },
   queryCache: new QueryCache({
     onError: (error) => toast.error(`${error.message}`),
   }),
