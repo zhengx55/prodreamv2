79843470dfd8e6f4e46a2d8ccb331763d68a468a
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-04-15 12:59:17 -0400

    Update GoogleDivider text in login and signup pages

--- app/[lang]/(auth)/login/page.tsx
+++ app/[lang]/(auth)/login/page.tsx
@@ -33,7 +33,7 @@ export default async function Page({
           <div className='flex-center relative my-10'>
             <Separator orientation='horizontal' className='bg-shadow-border' />
             <p className='small-regular absolute bg-white px-2 text-neutral-300'>
-              {dict.Auth.Login.GoogleDivider}
+              {dict.Auth.Signup.GoogleDivider}
             </p>
           </div>
           <LoginForm t={dict.Auth} lang={lang} />
