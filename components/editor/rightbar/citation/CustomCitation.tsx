bebe2668f887348557c23dd32b86e27e69155b3f
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-04-22 19:06:32 -0400

    Refactor citation form components in editor/rightbar/citation folder

--- components/editor/rightbar/citation/CustomCitation.tsx
+++ components/editor/rightbar/citation/CustomCitation.tsx
@@ -1,11 +1,32 @@
+import Spacer from '@/components/root/Spacer';
+import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { EditorDictType } from '@/types';
+import { useCitation } from '@/zustand/store';
+import { ChevronLeft } from 'lucide-react';
+import { memo } from 'react';
 import Book from './form/Book';
 import Journal from './form/Journal';
 import Website from './form/Website';
 
-const CustomCitation = () => {
+const CustomCitation = ({ t }: { t: EditorDictType }) => {
+  const updateShowCreateCitation = useCitation(
+    (state) => state.updateShowCreateCitation
+  );
   return (
     <section className='relative flex h-full w-full flex-col overflow-hidden'>
+      <div className='flex items-center gap-x-2'>
+        <Button
+          role='button'
+          variant={'icon'}
+          onClick={() => updateShowCreateCitation(false)}
+          className='size-max p-0.5'
+        >
+          <ChevronLeft size={20} />
+        </Button>
+        <h2 className='title-medium'>Add Customzied Citaitons</h2>
+      </div>
+      <Spacer y='20' />
       <Tabs defaultValue='Website' className='h-full w-full overflow-y-auto'>
         <TabsList className='h-8 w-full justify-start gap-x-2 rounded-none border-b border-gray-200 p-0'>
           <TabsTrigger
@@ -40,4 +61,4 @@ const CustomCitation = () => {
     </section>
   );
 };
-export default CustomCitation;
+export default memo(CustomCitation);
--- components/editor/rightbar/citation/SearchList.tsx
+++ components/editor/rightbar/citation/SearchList.tsx
@@ -7,6 +7,7 @@ import { EditorDictType } from '@/types';
 import { useCitation } from '@/zustand/store';
 import { useQuery } from '@tanstack/react-query';
 import { memo, useCallback, useEffect, useState } from 'react';
+import Title from '../Title';
 import { SearchCitationCard } from './CitationCard';
 import SearchBar from './SearchBar';
 
@@ -45,38 +46,41 @@ const SearchList = ({ t }: { t: EditorDictType }) => {
   }, [citationResult]);
 
   return (
-    <section className='relative flex flex-1 flex-col overflow-visible overflow-y-auto'>
-      <Spacer y='10' />
-      <SearchBar
-        t={t}
-        setResult={memopSetSearchResult}
-        setKeyword={setKeyword}
-      />
-      <Button
-        className='w-max bg-transparent px-2 text-violet-500 hover:underline'
-        variant={'ghost'}
-        onClick={() => {
-          updateShowCreateCitation(true);
-        }}
-      >
-        {t.Citation.customized}
-      </Button>
-      <Spacer y='10' />
-      <div className='flex h-[calc(100%_-115px)] w-full flex-col gap-y-8 overflow-y-auto'>
-        {isError ? null : isPending ? (
-          <Loading />
-        ) : (
-          searchResult &&
-          searchResult?.map((item, index) => (
-            <SearchCitationCard
-              index={index}
-              key={`citation-search${index}`}
-              item={item}
-            />
-          ))
-        )}
-      </div>
-    </section>
+    <>
+      <Title t={t} />
+      <section className='relative flex flex-1 flex-col overflow-visible overflow-y-auto'>
+        <Spacer y='10' />
+        <SearchBar
+          t={t}
+          setResult={memopSetSearchResult}
+          setKeyword={setKeyword}
+        />
+        <Button
+          className='w-max bg-transparent px-2 text-violet-500 hover:underline'
+          variant={'ghost'}
+          onClick={() => {
+            updateShowCreateCitation(true);
+          }}
+        >
+          {t.Citation.customized}
+        </Button>
+        <Spacer y='10' />
+        <div className='flex h-[calc(100%_-115px)] w-full flex-col gap-y-8 overflow-y-auto'>
+          {isError ? null : isPending ? (
+            <Loading />
+          ) : (
+            searchResult &&
+            searchResult?.map((item, index) => (
+              <SearchCitationCard
+                index={index}
+                key={`citation-search${index}`}
+                item={item}
+              />
+            ))
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 export default memo(SearchList);
