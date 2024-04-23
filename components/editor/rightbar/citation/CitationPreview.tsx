d122e9e54f9c3a49b0524832a52b939ef1be96dc
Xin Zheng <36489203+zhengx55@users.noreply.github.com>
2024-04-23 16:46:34 -0400

    Refactor citation form components in editor/rightbar/citation/CitationPreview.tsx

--- components/editor/rightbar/citation/CitationPreview.tsx
+++ components/editor/rightbar/citation/CitationPreview.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button';
-import { DialogContent } from '@/components/ui/dialog';
+import { DialogClose, DialogContent } from '@/components/ui/dialog';
 import { ICitation } from '@/query/type';
-import { Download } from 'lucide-react';
+import { Download, X } from 'lucide-react';
 import type { Route } from 'next';
 import Link from 'next/link';
 import { memo } from 'react';
@@ -10,58 +10,50 @@ type Props = { item: ICitation };
 
 const CitationPreview = ({ item }: Props) => {
   return (
-    <DialogContent className='flex h-[500px] bg-white sm:rounded md:w-[950px] 2xl:h-[700px]'>
-      <div className='flex-between w-1/2 flex-col'>
-        <div className='flex flex-col gap-y-4'>
-          <h1 className='h3-semibold'>{item.article_title}</h1>
-          {/* {item.area?.length > 0 && (
-            <div className='flex items-center gap-x-2'>
-              {item.area.map((keyword, idx) => (
-                <span
-                  className='subtle-regular rounded bg-violet-500/10 p-1.5 text-violet-500'
-                  key={`keyword-${idx}`}
-                >
-                  {keyword}
-                </span>
-              ))}
-            </div>
-          )} */}
-
-          <p className='subtle-regular text-shadow-100'>
-            Authors: {item.publication}
-          </p>
-
-          {item.publish_date.year &&
-            (item.publish_date.month && item.publish_date.month ? (
-              <p className='subtle-regular text-shadow-100'>
-                Published Date: {item.publish_date.day}{' '}
-                {item.publish_date.month} {item.publish_date.year}
-              </p>
-            ) : (
-              <p className='subtle-regular text-shadow-100'>
-                Published: {item.publish_date.year}
-              </p>
-            ))}
-          {item.journal_title && (
+    <DialogContent className='flex bg-white sm:rounded md:w-[650px]'>
+      <div className='flex flex-col gap-y-4'>
+        <h1 className='h3-semibold w-4/5'>{item.article_title}</h1>
+        <DialogClose>
+          <span className='flex-center absolute right-6 top-6 size-6 rounded-full bg-red-500'>
+            <X className='text-white' size={18} />
+          </span>
+        </DialogClose>
+        <p className='subtle-regular text-shadow-100'>
+          Authors: {item.publication}
+        </p>
+        {item.publish_date.year &&
+          (item.publish_date.month && item.publish_date.month ? (
             <p className='subtle-regular text-shadow-100'>
-              {item.journal_title}{' '}
-              {item.doi && (
-                <Link
-                  className='text-violet-500'
-                  href={`https://doi.org/${item.doi}`}
-                  target='_blank'
-                >
-                  | {item.doi}
-                </Link>
-              )}
+              Published Date: {item.publish_date.day} {item.publish_date.month}{' '}
+              {item.publish_date.year}
             </p>
-          )}
-        </div>
+          ) : (
+            <p className='subtle-regular text-shadow-100'>
+              Published: {item.publish_date.year}
+            </p>
+          ))}
+        {item.journal_title && (
+          <p className='subtle-regular text-shadow-100'>
+            {item.journal_title}{' '}
+            {item.doi && (
+              <Link
+                className='text-violet-500'
+                href={`https://doi.org/${item.doi}`}
+                target='_blank'
+              >
+                | {item.doi}
+              </Link>
+            )}
+          </p>
+        )}
+        {item.snippet && (
+          <p className='text-[14px] leading-relaxed'>{item.snippet}</p>
+        )}
         <div className='flex-between w-full pr-4'>
           <div className='flex items-center gap-x-2'>
             {item.pdf_url && (
               <Link passHref href={item.pdf_url as Route} target='_blank'>
-                <Button className='h-max rounded-xl bg-violet-500/10 text-violet-500'>
+                <Button className='h-max rounded-xl bg-violet-500/10 text-violet-500 hover:bg-violet-500/20 active:bg-violet-500/20'>
                   PDF
                   <Download size={16} className='text-violet-500' />
                 </Button>
@@ -73,20 +65,6 @@ const CitationPreview = ({ item }: Props) => {
             <p className='subtle-regular text-shadow-100'>Cited by</p>
           </div>
         </div>
-      </div>
-      <div className='flex w-1/2 flex-col gap-y-2 overflow-y-auto'>
-        {item.tldr && (
-          <>
-            <h2 className='title-regular text-neutral-400'>Summary</h2>
-            <p className='text-[14px] leading-relaxed'>{item.tldr}</p>
-          </>
-        )}
-        {item.snippet && (
-          <>
-            <h2 className='title-regular text-neutral-400'>Abstract</h2>
-            <p className='text-[14px] leading-relaxed'>{item.snippet}</p>
-          </>
-        )}
       </div>
     </DialogContent>
   );
