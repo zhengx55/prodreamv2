9fe93216821e6fb9c4de0eca85d2bbe18a768e85
ChinaBigPan <595554725@qq.com>
2024-07-11 23:29:10 +0800

    type check

--- components/onboarding/CenteredImage.tsx
+++ components/onboarding/CenteredImage.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 
 export const CenteredImage = () => (
-    <Image
-        src="/onboarding/dreami_test.png"
-        alt="centered character"
-        width={572}
-        height={682}
-        className="absolute top-1/3 animate-float"
-    />
+  <Image
+    src='/onboarding/dreami_test.png'
+    alt='centered character'
+    width={572}
+    height={682}
+    className='animate-float absolute top-1/3'
+  />
 );
