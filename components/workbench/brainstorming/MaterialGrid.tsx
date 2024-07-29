fb55fffc1bfc0222bc93070b897951aca1a45dcc
zhengx55 <williamzhxin@gmail.com>
2024-07-29 20:57:09 +0800

    feat: Add new page component for brainstorming

---
+++ components/workbench/brainstorming/MaterialGrid.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const MaterialGrid = (props: Props) => {
+  return <div>MaterialGrid</div>;
+};
+
+export default MaterialGrid;
---
+++ components/workbench/brainstorming/MaterialList.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const MaterialList = (props: Props) => {
+  return <div>MaterialList</div>;
+};
+
+export default MaterialList;
