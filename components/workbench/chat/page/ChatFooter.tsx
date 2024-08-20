6f813466a4dd703fd0babe4434a0feedc86b66e6
zhengx55 <williamzhxin@gmail.com>
2024-08-21 01:14:35 +0800

    refactor: Add lazy loading for images in MaterialGridItem, ChatFooter, ChatMessageItem, DraftGridItem, and OutlineGridItem components

--- components/workbench/chat/page/ChatFooter.tsx
+++ components/workbench/chat/page/ChatFooter.tsx
@@ -1,15 +1,59 @@
+'use client';
+import Icon from '@/components/root/Icon';
 import { Button } from '@/components/ui/button';
+import { ICONS } from '@/constant/chat_agent_constant';
+import { useAgent } from '@/zustand/store';
 import { Layers } from 'lucide-react';
 import { memo } from 'react';
 import ChatInputField from './ChatInputField';
 
+type IconButtonProps = {
+  alt: string;
+  src: string;
+  onClick?: () => void;
+};
+
+const IconButton = ({ alt, src, onClick = () => {} }: IconButtonProps) => (
+  <Button
+    onClick={onClick}
+    variant='icon'
+    className='size-max p-1'
+    role='button'
+  >
+    <Icon
+      alt={alt}
+      src={src}
+      width={20}
+      height={20}
+      priority
+      className='size-5'
+    />
+  </Button>
+);
+
 const ChatFooter = () => {
+  const clearChatSession = useAgent((state) => state.clearSession);
   return (
     <footer className='w-[860px] space-y-2.5 self-center pt-4'>
-      <Button role='button' className='px-2 text-sm' variant='outline'>
-        <Layers size={20} className='text-indigo-500' />
-        Common guidance
-      </Button>
+      <div className='flex-between'>
+        <Button role='button' className='px-2 text-sm' variant='outline'>
+          <Layers size={20} className='text-indigo-500' />
+          Common guidance
+        </Button>
+        <div className='flex gap-x-2'>
+          {ICONS.common.map(({ alt, src }) => (
+            <IconButton
+              key={alt}
+              alt={alt}
+              src={src}
+              onClick={
+                alt === 'new' ? () => clearChatSession('chat') : undefined
+              }
+            />
+          ))}
+        </div>
+      </div>
+
       <ChatInputField />
     </footer>
   );
